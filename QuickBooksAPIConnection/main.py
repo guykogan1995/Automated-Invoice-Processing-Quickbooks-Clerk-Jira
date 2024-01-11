148ea455c88447d2ed77f4b86b4a0d2bf257e6c7
guykogan1995 <guykogan@u.boisestate.edu>
2024-01-10 17:03:34 -0700

    Working on Sandbox

--- QuickBooksAPIConnection/main.py
+++ QuickBooksAPIConnection/main.py
@@ -1,46 +1,80 @@
+import time
+import schedule
 from intuitlib.client import AuthClient
-import requests
+from intuitlib.enums import Scopes
 import base64
+import requests
 
-TOKEN_URL = 'type in'
-CLIENT_ID = 'type in'
-CLIENT_SECRET = 'type in'
-AUTHORIZATION_CODE = 'type in'
+global transactions
+payed_transactions = []
+
+
+def refresh_access_token(refresh_token, client_id, client_secret):
+    token_endpoint = 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer'
+    auth_header2 = base64.b64encode(f'{client_id}:{client_secret}'.encode()).decode()
+    headers2 = {
+        'Authorization': f'Basic {auth_header2}',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token
+    }
+    response2 = requests.post(token_endpoint, headers=headers2, data=data)
+    if response2.status_code == 200:
+        new_tokens = response2.json()
+        return new_tokens['access_token'], new_tokens['refresh_token']
+    else:
+        raise Exception(f"Failed to refresh token: {response.text}")
+
+
+def get_transactions():
+    global transactions
+    response2 = requests.get(url, headers=headers)
+    invoices2 = response2.json()
+    transactions = invoices2['QueryResponse']['Invoice']
+
+
+CLIENT_ID = 'ABRfb8HlI2YzXziLnnXqooLXiqI24kKyGPZIgy3X9kkeZ289Tq'
+CLIENT_SECRET = 'pQAibk5sRKSbu6Fkk0ujs7vzrqsE0JvbKV2KTcsu'
 REDIRECT_URI = 'https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl'
 SANDBOX_ENVIRONMENT = 'sandbox'
-REALM_ID = '4620816365380591410'
 
-# Set up AuthClient for the authorization URL
 auth_client = AuthClient(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
     redirect_uri=REDIRECT_URI,
     environment=SANDBOX_ENVIRONMENT,
 )
+url = auth_client.get_authorization_url([Scopes['ACCOUNTING']])
+print(url)
 
-auth_header = f"{CLIENT_ID}:{CLIENT_SECRET}"
-encoded_auth_header = base64.b64encode(auth_header.encode('utf-8')).decode('utf-8')
-auth_value = f'Basic {encoded_auth_header}'
+auth_code = 'AB11704872589jng2pa74DZmEAfGPTzKqQ5tha7u7IMZkK0Lnv'
+realm_id = '4620816365380591410'
+auth_client.get_bearer_token(auth_code, realm_id=realm_id)
 
+base_url = 'https://sandbox-quickbooks.api.intuit.com'
+url = '{0}/v3/company/{1}/query?query=select * from Invoice'.format(base_url, auth_client.realm_id)
+auth_header = 'Bearer {0}'.format(auth_client.access_token)
 headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Accept': 'application/json',
-    'Authorization': auth_value
+    'Authorization': auth_header,
+    'Accept': 'application/json'
 }
-
-data = {
-    'grant_type': 'authorization_code',
-    'code': AUTHORIZATION_CODE,
-    'redirect_uri': REDIRECT_URI,
-    'environment': SANDBOX_ENVIRONMENT,
-    'realmId': REALM_ID  # Include the realm ID in the request
-}
-
-response = requests.post(TOKEN_URL, headers=headers, data=data)
-
+response = requests.get(url, headers=headers)
 if response.status_code == 200:
-    access_token = response.json().get('access_token')
-    print(f"Access Token: {access_token}")
+    schedule.every(30).minutes.do(refresh_access_token, auth_client.refresh_token, CLIENT_ID, CLIENT_SECRET)
+    schedule.every(30).minutes.do(get_transactions)
+    get_transactions()
+    for transaction in transactions:
+        if transaction['Balance'] == 0.0:
+            invoice_dictionary = {"ID": transaction["Id"], "Total Amount": transaction["TotalAmt"],
+                                  "Customer Name": transaction["CustomerRef"]["name"],
+                                  "Bill Email": transaction["BillEmail"]["Address"]}
+            payed_transactions.append(invoice_dictionary)
+    print(payed_transactions)
+    print(len(payed_transactions))
+    while True:
+        schedule.run_pending()
+        time.sleep(60 * 10)
 else:
-    print(f"Failed to retrieve access token. Status code: {response.status_code}")
-    print(response.text)
+    print(f"Failed to fetch invoices: {response.text}")
